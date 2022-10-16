4828c661658e6fef1418079c48e6cec5b746cf83
Martin Oppitz <github@martinoppitz.com>
2022-10-16 05:33:15 +0200

    chore: remove pwa

--- packages/create-kolibri/templates/react/src/react.main.tsx
+++ packages/create-kolibri/templates/react/src/react.main.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter as Router } from 'react-router-dom';
 
 import { AppComponent } from './components/app/component';
 
@@ -13,11 +12,7 @@ register(BMF, defineCustomElements)
 		const htmlElement: HTMLElement | null = document.querySelector<HTMLDivElement>('div#app');
 		if (htmlElement instanceof HTMLElement) {
 			const root = createRoot(htmlElement);
-			root.render(
-				<Router>
-					<AppComponent />
-				</Router>
-			);
+			root.render(<AppComponent />);
 		}
 	})
 	.catch(console.warn);
